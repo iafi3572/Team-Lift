42c0ce0a0c539a7f808e164e27c98fd3552dcade
Samuel Hagen <hagensa615@gmail.com>
2025-03-31 22:53:36 -0600

    Created the exercises page and created the API route

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -3,16 +3,17 @@
 // <!-- Section 1 : Import Dependencies -->
 // *****************************************************
 
-const express = require('express'); // To build an application server or API
+const express = require("express"); // To build an application server or API
 const app = express();
-const handlebars = require('express-handlebars');
-const Handlebars = require('handlebars');
-const path = require('path');
-const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
-const bodyParser = require('body-parser');
-const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
-const bcrypt = require('bcryptjs'); //  To hash passwords
-const req = require('express/lib/request');
+const handlebars = require("express-handlebars");
+const Handlebars = require("handlebars");
+const path = require("path");
+const pgp = require("pg-promise")(); // To connect to the Postgres DB from the node server
+const bodyParser = require("body-parser");
+const session = require("express-session"); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require("bcryptjs"); //  To hash passwords
+const req = require("express/lib/request");
+const axios = require("axios").default;
 
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
@@ -20,14 +21,14 @@ const req = require('express/lib/request');
 
 // create `ExpressHandlebars` instance and configure the layouts and partials dir.
 const hbs = handlebars.create({
-  extname: 'hbs',
-  layoutsDir: __dirname + '/views/layouts',
-  partialsDir: __dirname + '/views/partials',
+  extname: "hbs",
+  layoutsDir: __dirname + "/views/layouts",
+  partialsDir: __dirname + "/views/partials",
 });
 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
+  host: "db", // the database server
   port: 5432, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
@@ -38,12 +39,12 @@ const db = pgp(dbConfig);
 
 // test your database
 db.connect()
-  .then(obj => {
-    console.log('Database connection successful'); // you can view this message in the docker compose logs
+  .then((obj) => {
+    console.log("Database connection successful"); // you can view this message in the docker compose logs
     obj.done(); // success, release the connection;
   })
-  .catch(error => {
-    console.log('ERROR:', error.message || error);
+  .catch((error) => {
+    console.log("ERROR:", error.message || error);
   });
 
 // *****************************************************
@@ -51,9 +52,9 @@ db.connect()
 // *****************************************************
 
 // Register `hbs` as our view engine using its bound `engine()` function.
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
 
 // initialize session variables
@@ -77,11 +78,31 @@ app.use(
 
 // TODO - Include your API routes here
 
+app.get("/exercises", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://exercisedb.p.rapidapi.com/exercises",
+    headers: {
+      "X-RapidAPI-Key": process.env.API_KEY,
+      "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+    },
+  };
 
+  try {
+    const { data } = await axios.request(options);
+    console.log(data);
+    res.render("pages/exercises.hbs", { data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).render("pages/exercises.hbs", {
+      exercises: [],
+    });
+  }
+});
 
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
 app.listen(3000);
-console.log('Server is listening on port 3000');
+console.log("Server is listening on port 3000");
